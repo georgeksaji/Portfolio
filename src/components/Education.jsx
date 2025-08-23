8e4f8bfb58667339b6e0328728bb011e8a55506a
georgeksaji <georgeksaji14@gmail.com>
2025-08-23 15:00:41 +0530

    Revert "Working on educationv2"
    This reverts commit 8e8d55bd8d658c1e2b8e23888165194a33489fee.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import ShinyText from "./ShinyText/ShinyText";
-import CertificatesStack from "./CertificatesStack";
 
 export default function Education() {
   return (
     <section
       id="about"
-      className="row target-section relative z-20 h-screen flex items-center px-6"
+      className="row target-section relative z-20 min-h-screen flex items-center px-6"
       data-parallax="scroll"
     >
-      <div className="w-full flex flex-col">
+      <div className="max-w-6xl w-full flex flex-col md:flex-row gap-12">
         {/* Left column */}
-        <div className="large-full mb-10 md:mb-30">
-          <ShinyText
-            text="About Me"
+        <div className="flex-1 flex flex-col justify-center">
+                    <ShinyText
+            text="Education"
             disabled={false}
             speed={3}
             className="custom-class text-5xl mb-8"
           />
-          <br />
+            {/* <p className="text-[#b6b6b6] mt-6 mb-8 max-w-md">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.
+            </p> */}
+          {/* Certifications box */}
+          <div className="bg-[#e2b6b0] rounded-md p-6 mt-8 max-w-md">
+            <h3 className="text-lg mb-2">Certifications</h3>
+            <hr className="border-[#b6b6b6] mb-2" />
+            <div className="mb-1">2019 - DESIGN Magazine, Site of the Month</div>
+            <div>2015 - Innovative Design of the Year</div>
+          </div>
         </div>
         {/* Timeline: alternate left/right */}
         <div className="flex-1 flex flex-col justify-center relative">
@@ -28,13 +36,12 @@ export default function Education() {
             {/* MCA - left */}
             <div className="flex w-full items-start relative">
               <div className="flex-1 flex flex-col items-end pr-12">
-                <span className="text-[#b6b6b6] text-lg">2023 - present</span>
-                <h2 className="text-2xl text-[#b6b6b6] font-semibold mt-1 text-right about-label-text">
-                  Master of Computer Applications (MCA)
-                </h2>
-                <div className="text-[#b6b6b6] text-base mb-2 text-right about-label-text">
-                  Rajagiri College of Social Sciences, Kalamassery
-                </div>
+                <span>2023 - Present</span>
+                <h2 className="text-2xl text-white font-semibold mt-1 text-right about-label-text">Master of Computer Applications (MCA)</h2>
+                <div className="text-base mb-2 text-right about-label-text">Rajagiri College of Social Sciences, Kalamassery</div>
+                {/* <p className="text-[#b6b6b6] max-w-lg text-right">
+                  Pursuing MCA with a focus on full-stack development, web technologies, and software engineering. Coursework includes advanced programming, databases, and project management.
+                </p> */}
               </div>
               <div className="w-1/12 flex flex-col items-center relative z-10">
                 {/* Timeline dot overlays vertical line */}
@@ -48,15 +55,10 @@ export default function Education() {
               <div className="w-1/12 flex flex-col items-center relative z-10">
                 <div className="bg-[#e2b6b0] rounded-full border-2 border-[#b6b6b6] w-4 h-4" />
               </div>
-
               <div className="flex-1 flex flex-col items-start pl-12">
                 <span className="text-[#b6b6b6] text-lg">2020 - 2023</span>
-                <h2 className="text-2xl text-[#b6b6b6] font-semibold mt-1 about-label-text">
-                  Bachelor of Computer Applications (BCA)
-                </h2>
-                <div className="text-[#b6b6b6] text-base mb-2 about-label-text">
-                  Rajagiri College of Social Sciences, Kalamassery
-                </div>
+                <h2 className="text-2xl text-white font-semibold mt-1 about-label-text">Bachelor of Computer Applications (BCA)</h2>
+                <div className="text-[#b6b6b6] text-base mb-2 about-label-text">Rajagiri College of Social Sciences, Kalamassery</div>
                 {/* <p className="text-[#b6b6b6] max-w-lg">
                   Graduated with a strong foundation in programming, algorithms, and database management. Participated in various tech fests and coding competitions.
                 </p> */}
